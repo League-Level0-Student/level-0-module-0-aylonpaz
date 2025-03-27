6d890611595163e4443a4101626b6ce6c30fe446
nolanbooth <nb@nb.org>
2025-03-26 19:59:00 -0700

    I made a popup and a robot animation. I am currently working on a madlib.

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -7,15 +7,15 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
-		
+		JOptionPane.showMessageDialog(null,prompt);
 		// Get the player to enter an adjective
-
+		String adjective = JOptionPane.showInputDialog("Enter an adjective");
 		// Get the player to enter a type of liquid
-
+		String liquid = JOptionPane.showInputDialog("Enter a liquid");
 		// Get the player to enter a body part
-
+		String bodyPart = JOptionPane.showInputDialog("Enter a body part");
 		// Get the player to enter a verb
-
+		String verb = JOptionPane.showInputDialog("Enter a verb");
 		// Get the player to enter a place
 
 		// The story below has has been written as a group of Strings joined together by + signs.
